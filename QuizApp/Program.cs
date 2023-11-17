018447395e370695e46b00e609b8dae6e567c9ab
andrzej-herman <aherman@san.edu.pl>
2023-11-17 12:46:49 +0100

    Add project

---
+++ QuizApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using QuizApp.backend;
+Console.WriteLine(int.MaxValue);
+Console.WriteLine(long.MaxValue);
+
+
+// string
+// int
+// boolean
+// DateTime
+
+bool f = true; 
+DateTime data = DateTime.Now;
+Console.WriteLine(data);
+Console.ReadLine();
+
+
+
+
+
